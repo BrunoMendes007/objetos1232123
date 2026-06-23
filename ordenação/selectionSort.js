a786b4152752dc5c8a859df6bf8a03c9ed408a56
BrunoMendes007 <bruno.neduziak.mendes@escola.pr.gov.br>
2026-06-23 11:38:48 +0000

    menorvalor

---
+++ ordenação/selectionSort.js
@@ -0,0 +1,9 @@
+const livros = require('./listaLivros');
+const menorValor = require('./menorValor');
+function menorValor(arrProdutos, posicaoInicial){
+
+}
+let maisBarato = 0;
+for(let atual = 0; atual < livros.lenght; atual++){
+
+}
